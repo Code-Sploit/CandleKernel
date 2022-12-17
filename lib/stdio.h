bf1611de38864537b70dd25a7e917c243204ab6a
Code-Sploit <64798467+Code-Sploit@users.noreply.github.com>
2022-12-17 22:59:58 +0100

    Update stdio.h

--- lib/stdio.h
+++ lib/stdio.h
@@ -4,8 +4,12 @@
 
 #define SCREEN_SIZE (SCREEN_SIZE_COLS * SCREEN_SIZE_ROWS * SCREEN_SIZE_BLEN)
 
+#define ATTR_BYTE_BLK_ON_BLK 0x00
 #define ATTR_BYTE_LG_ON_BLK 0x07
 
 static char *VIDEO_MEMORY_POINTER = (char*)0xb8000;
 
+static int VIDEO_POINTER_SIZE = 0;
+
+void kstd_clear(void);
 void kstd_write(const char *__sptr);
