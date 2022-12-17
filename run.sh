3c221c6b6942fb139d372f0e0b309db3c552e970
Code-Sploit <64798467+Code-Sploit@users.noreply.github.com>
2022-12-17 21:43:18 +0100

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+BUILDDIR='build'
+CONFDIR='conf'
+SRCDIR='src'
+ASMDIR='asm'
+LIBDIR='lib'
+
+mkdir -p $BUILDDIR
+mkdir -p $CONFDIR
+mkdir -p $SRCDIR
+mkdir -p $ASMDIR
+mkdir -p $LIBDIR
+
+nasm -f elf32 $ASMDIR/kernel.asm -o $BUILDDIR/kasm.o
+
+gcc -m32 -c $SRCDIR/kernel.c -o $BUILDDIR/kernel.o
+gcc -m32 -c $LIBDIR/stdio.c -o $BUILDDIR/stdio.o
+
+ld -m elf_i386 -T $CONFDIR/linker.ld -o kernel $BUILDDIR/kasm.o $BUILDDIR/kernel.o $BUILDDIR/stdio.o
+
+echo "-------"
+echo "Compilation succeeded!"
+echo "-------"
+
+qemu-system-i386 -kernel kernel
