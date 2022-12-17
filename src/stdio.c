6f31f343ee476d3367710c847cd0c556efaca7f2
Code-Sploit <64798467+Code-Sploit@users.noreply.github.com>
2022-12-17 22:58:18 +0100

    Update stdio.c

--- src/stdio.c
+++ src/stdio.c
@@ -3,29 +3,49 @@
 */
 
 #include "../lib/stdio.h"
+#include "../lib/stdlib.h"
 
-void kstd_write(const char *__sptr)
+void kstd_set_vidptr(int __size)
 {
-	unsigned int __ip = 0;
+	VIDEO_POINTER_SIZE = __size;
+}
+
+void kstd_clear(void)
+{
 	unsigned int __jp = 0;
 
 	while (__jp < (SCREEN_SIZE))
 	{
 		VIDEO_MEMORY_POINTER[__jp] = ' ';
-		VIDEO_MEMORY_POINTER[__jp + 1] = ATTR_BYTE_LG_ON_BLK;
+		VIDEO_MEMORY_POINTER[__jp + 1] = ATTR_BYTE_BLK_ON_BLK;
 
 		__jp = __jp + 2;
 	}
+}
 
-	__jp = 0;
+void kstd_write(const char *__sptr)
+{
+	unsigned int __string_len = (kstd_strlen(__sptr) * 2);
+	unsigned int __ip         = VIDEO_POINTER_SIZE;
+	unsigned int __jp	  = VIDEO_POINTER_SIZE;
+	unsigned int __kp	  = 0;
 
-	while (__sptr[__jp] != '\0')
+	while (__ip < (VIDEO_POINTER_SIZE + __string_len))
 	{
-		VIDEO_MEMORY_POINTER[__ip] = __sptr[__jp];
-		VIDEO_MEMORY_POINTER[__ip + 2] = ATTR_BYTE_LG_ON_BLK;
-
-		++__jp;
-
+		VIDEO_MEMORY_POINTER[__ip] = ' ';
+		VIDEO_MEMORY_POINTER[__ip + 1] = ATTR_BYTE_LG_ON_BLK;
+		
 		__ip = __ip + 2;
 	}
+
+	while (__jp < (VIDEO_POINTER_SIZE + __string_len))
+	{
+		VIDEO_MEMORY_POINTER[__jp] = __sptr[__kp];
+		VIDEO_MEMORY_POINTER[__jp + 1] = ATTR_BYTE_LG_ON_BLK;
+
+		__jp = __jp + 2;
+		__kp = __kp + 1;
+	}
+	
+	kstd_set_vidptr(VIDEO_POINTER_SIZE + __string_len);
 }
