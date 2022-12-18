f5ff7b2854ff7345d9b6b445c30731ff782b9e7f
Code-Sploit <64798467+Code-Sploit@users.noreply.github.com>
2022-12-18 01:29:36 -0600

    Update kernel.c

--- src/kernel.c
+++ src/kernel.c
@@ -7,9 +7,15 @@
 
 void kmain(void)
 {
-	const char *_msg1 = "Booting up: CandleKernel!";
+	const char *_msg1 = "Booting up: CandleKernel!\n";
+	const char *_msg2 = "Hello world!\n";
+	const char *_msg3 = "Newline test\n";
+
+	kstd_clear();
 
 	kstd_write(_msg1);
+	kstd_write(_msg2);
+	kstd_write(_msg3);
 
 	return;
 }
