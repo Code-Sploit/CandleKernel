a8343378b15e2cae1bc00e091fbd2463112f2ef2
Code-Sploit <64798467+Code-Sploit@users.noreply.github.com>
2022-12-18 01:37:22 -0600

    Update stdio.c

--- src/stdio.c
+++ src/stdio.c
@@ -1,81 +1,25 @@
-/*
- * stdio.c
-*/
+#define SCREEN_SIZE_COLS 80
+#define SCREEN_SIZE_ROWS 25
+#define SCREEN_SIZE_BLEN 2
 
-#include "../lib/stdio.h"
-#include "../lib/stdlib.h"
+#define SCREEN_SIZE (SCREEN_SIZE_COLS * SCREEN_SIZE_ROWS * SCREEN_SIZE_BLEN)
 
-void kstd_set_vidptr(int __size)
-{
-	VIDEO_POINTER_SIZE = __size;
-}
+#define ATTR_BYTE_BLK_ON_BLK 0x00   /* BLACK ON BLACK */
+#define ATTR_BYTE_BLU_ON_BLK 0x01   /* BLUE ON BLACK */
+#define ATTR_BYTE_GRN_ON_BLK 0x02   /* GREEN ON BLACK */
+#define ATTR_BYTE_AQU_ON_BLK 0x03   /* AQUA ON BLACK */
+#define ATTR_BYTE_RED_ON_BLK 0x04   /* RED ON BLACK */
+#define ATTR_BYTE_MAG_ON_BLK 0x05   /* MAGENTA ON BLACK */
+#define ATTR_BYTE_ORG_ON_BLK 0x06   /* ORANGE ON BLACK */
+#define ATTR_BYTE_LGR_ON_BLK 0x07   /* L-GRAY ON BLACK */
+#define ATTR_BYTE_DGR_ON_BLK 0x08   /* D-GRAY ON BLACK */
+#define ATTR_BYTE_DBL_ON_BLK 0x09   /* DODGER-BLUE ON BLACK */
 
-void kstd_free_space(int __size)
-{
-	unsigned int cursor = VIDEO_POINTER_SIZE;
+#define ATTR_BYTE_TEXT_COLOR_STD ATTR_BYTE_LGR_ON_BLK
 
-	while (cursor < __size)
-	{
-		VIDEO_MEMORY_POINTER[cursor] = ' ';
-		VIDEO_MEMORY_POINTER[cursor + 1] = ATTR_BYTE_BLK_ON_BLK;
+static char *VIDEO_MEMORY_POINTER = (char*)0xb8000;
 
-		cursor = cursor + 2;
-	}
-}
+static int VIDEO_POINTER_SIZE = 0;
 
-void kstd_clear(void)
-{
-	unsigned int cursor = 0;
-
-	while (cursor < (SCREEN_SIZE))
-	{
-		VIDEO_MEMORY_POINTER[cursor] = ' ';
-		VIDEO_MEMORY_POINTER[cursor + 1] = ATTR_BYTE_BLK_ON_BLK;
-
-		cursor = cursor + 2;
-	}
-}
-
-void kstd_handle_escape(char __p)
-{
-	switch (__p)
-	{
-		case '\n':
-		{
-			int offset_to_eol = (SCREEN_SIZE_COLS * SCREEN_SIZE_BLEN) - (VIDEO_POINTER_SIZE % SCREEN_SIZE_COLS);
-
-			VIDEO_POINTER_SIZE = VIDEO_POINTER_SIZE + offset_to_eol;
-		}
-	}
-}
-
-void kstd_write(const char *__sptr)
-{
-	unsigned int strlen  = kstd_strlen(__sptr);
-
-	unsigned int viditer = VIDEO_POINTER_SIZE;
-	unsigned int striter = 0;
-
-	kstd_free_space(strlen);
-
-	while (striter < strlen)
-	{
-		if (__sptr[striter] == '\n')
-		{
-			kstd_handle_escape(__sptr[striter]);
-
-			viditer = viditer + 2;
-
-			striter++;
-
-			continue;
-		}
-
-		VIDEO_MEMORY_POINTER[viditer] = __sptr[striter];
-		VIDEO_MEMORY_POINTER[viditer + 1] = ATTR_BYTE_LG_ON_BLK;
-
-		viditer = viditer + 2;
-
-		striter++;
-	}
-}
+void kstd_clear(void);
+void kstd_write(const char *__sptr);
