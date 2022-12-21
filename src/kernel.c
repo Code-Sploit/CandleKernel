cc90f6ab495a64bbe221993344f626a19b67ae5a
Code-Sploit <64798467+Code-Sploit@users.noreply.github.com>
2022-12-21 08:13:01 -0600

    Memory tests

--- src/kernel.c
+++ src/kernel.c
@@ -3,8 +3,8 @@
 */
 
 #include "../lib/kernel.h"
-#include "../lib/stdlib.h"
 #include "../lib/stdio.h"
+#include "../lib/memory.h"
 
 void kmain(void)
 {
@@ -12,11 +12,17 @@ void kmain(void)
 	kstd_clear();
 
 	/* Initialize memory */
-	kstd_mem_init(NULL, (void *) 16000);
+	void *__KSTD_MEM_START_ADDR = kstd_mem_pre_alloc_blocks(20);
+	void *__KSTD_MEM_END_ADDR	= __KSTD_MEM_START_ADDR + (kstd_mem_pre_next_free(1) * KSTD_MEM_BLOCK_SIZE);
 
-	/* Test allocating a string */
+	kstd_mem_init(__KSTD_MEM_START_ADDR, __KSTD_MEM_END_ADDR);
 
-	char *str1 = kstd_mem_malloc(sizeof(char));
+	/* Test allocating & deallocating a string */
+	char *str = kstd_mem_malloc(sizeof(char));
+
+	kstd_strcat(str, "Hello, World!\n");
+	kstd_write(str);
+	kstd_mem_free(str);
 
 	return;
 }
