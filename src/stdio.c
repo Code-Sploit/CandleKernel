cd553b12a195674f4584e49a05bac4965e1f354e
Code-Sploit <64798467+Code-Sploit@users.noreply.github.com>
2022-12-21 08:14:16 -0600

    Add memory V2

--- src/stdio.c
+++ src/stdio.c
@@ -3,18 +3,7 @@
 */
 
 #include "../lib/stdio.h"
-
-int kstd_strlen(const char *__sptr)
-{
-	unsigned int __len = 0;
-
-	while (__sptr[__len] != '\0')
-	{
-		__len++;
-	}
-
-	return __len;
-}
+#include "../lib/memory.h"
 
 void kstd_set_vidptr(int __size)
 {
