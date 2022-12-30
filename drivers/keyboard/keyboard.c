6e35b0257f122248b20d6106b65113afed1e293a
Code-Sploit <64798467+Code-Sploit@users.noreply.github.com>
2022-12-30 07:55:47 -0600

    Create keyboard.c

---
+++ drivers/keyboard/keyboard.c
@@ -0,0 +1,58 @@
+#include "keyboard.h"
+
+#include "../console/console.h"
+#include "../pio/pio.h"
+#include "../isr/isr.h"
+
+#include "../../lib/stdio.h"
+
+const char *__kstd_keyboard_sc_name[] = {"ERROR", "Esc", "1", "2", "3", "4", "5", "6",
+                         "7", "8", "9", "0", "-", "=", "Backspace", "Tab", "Q", "W", "E",
+                         "R", "T", "Y", "U", "I", "O", "P", "[", "]", "Enter", "Lctrl",
+                         "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "`",
+                         "LShift", "\\", "Z", "X", "C", "V", "B", "N", "M", ",", ".",
+                         "/", "RShift", "Keypad *", "LAlt", "Spacebar"};
+
+const char __kstd_keyboard_sc_ascii[] = {'?', '?', '1', '2', '3', '4', '5', '6',
+                         '7', '8', '9', '0', '-', '=', '?', '?', 'Q', 'W', 'E', 'R', 'T', 'Y',
+                         'U', 'I', 'O', 'P', '[', ']', '?', '?', 'A', 'S', 'D', 'F', 'G',
+                         'H', 'J', 'K', 'L', ';', '\'', '`', '?', '\\', 'Z', 'X', 'C', 'V',
+                         'B', 'N', 'M', ',', '.', '/', '?', '?', '?', ' '};
+
+static void __kstd_keyboard_callback(registers_t *__regs)
+{
+    uint8 __scode = __kstd_inb(0x60);
+
+    if (__scode > SC_MAX) return;
+
+    if (__scode == BACKSPACE) {
+        if (__kstd_is_backspace_valid(__kstd_keyboard_buffer))
+        {
+            kstd_putchar('\b');
+        }
+    }
+    else if (__scode == ENTER)
+    {
+        kstd_newline();
+        
+        __kstd_console_run(__kstd_keyboard_buffer);
+
+        __kstd_keyboard_buffer[0] = '\0';
+
+        kstd_write("root@candleos > ");
+    }
+    else
+    {
+        char __ch     = __kstd_keyboard_sc_ascii[(int) __scode];
+        char __str[2] = {__ch, '\0'};
+
+        __kstd_append_char_to_string(__kstd_keyboard_buffer, __ch);;
+
+        kstd_write(__str);
+    }
+}
+
+void __kstd_enable_keyboard(void)
+{
+    __kstd_register_interrupt_handler(IRQ1, __kstd_keyboard_callback);
+}
