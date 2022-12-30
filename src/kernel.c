bacec2d6f9f7af044a478bcf1f67a43c18b47246
Code-Sploit <64798467+Code-Sploit@users.noreply.github.com>
2022-12-30 07:56:41 -0600

    Update kernel.c

--- src/kernel.c
+++ src/kernel.c
@@ -7,25 +7,34 @@
 #include "../lib/memory.h"
 #include "../lib/stdproc.h"
 
+#include "../drivers/idt/idt.h"
+#include "../drivers/isr/isr.h"
+#include "../drivers/keyboard/keyboard.h"
+
 void kmain(void)
 {
-	/* Clear the screen */
 	kstd_clear();
 
-	/* Initialize memory */
+	__kstd_isr_enable();
+	asm volatile("sti");
+	__kstd_enable_keyboard();
+
+	kstd_clear();
+
+	kstd_write("Initializing VGA buffer... [Done]\n");
+	kstd_write("Initializing ISR handler... [Done]\n");
+	kstd_write("Initializing Keyboard... [Done]\n");
+
+	/* Memory */
+
 	void *__KSTD_MEM_START_ADDR = kstd_mem_pre_alloc_blocks(20);
 	void *__KSTD_MEM_END_ADDR	= __KSTD_MEM_START_ADDR + (kstd_mem_pre_next_free(1) * KSTD_MEM_BLOCK_SIZE);
 
 	kstd_mem_init(__KSTD_MEM_START_ADDR, __KSTD_MEM_END_ADDR);
 
-	/* Process management testing */
+	kstd_write("Initializing 20 blocks of memory... [Done]\n");
 
-	__initialize_proc();
-
-	int pid = __create_proc(__PROCESS_RUNNING, 1);
-	int pid2 = __create_proc(__PROCESS_RUNNING, 1);
-
-	__print_procs();
+	kstd_write("\nWelcome to CandleOS\nAwaiting Commands...\nroot@candleos > ");
 
 	return;
 }
