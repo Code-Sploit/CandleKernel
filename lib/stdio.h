ea4074de50d580859bca5407c4a376290d759da9
Code-Sploit <64798467+Code-Sploit@users.noreply.github.com>
2022-12-31 04:00:52 -0600

    Move direct interaction with video memory in stdio.c to video driver

--- lib/stdio.h
+++ lib/stdio.h
@@ -1,35 +1,14 @@
 #ifndef STDIO_H
 #define STDIO_H
 
-#define SCREEN_SIZE_COLS 80
-#define SCREEN_SIZE_ROWS 25
-#define SCREEN_SIZE_BLEN 2
+#include "../drivers/vga/vga.h"
+#include "../lib/stdlib.h"
 
-#define SCREEN_SIZE (SCREEN_SIZE_COLS * SCREEN_SIZE_ROWS * SCREEN_SIZE_BLEN)
-
-#define ATTR_BYTE_BLK_ON_BLK 0x00   /* BLACK ON BLACK */
-#define ATTR_BYTE_BLU_ON_BLK 0x01   /* BLUE ON BLACK */
-#define ATTR_BYTE_GRN_ON_BLK 0x02   /* GREEN ON BLACK */
-#define ATTR_BYTE_AQU_ON_BLK 0x03   /* AQUA ON BLACK */
-#define ATTR_BYTE_RED_ON_BLK 0x04   /* RED ON BLACK */
-#define ATTR_BYTE_MAG_ON_BLK 0x05   /* MAGENTA ON BLACK */
-#define ATTR_BYTE_ORG_ON_BLK 0x06   /* ORANGE ON BLACK */
-#define ATTR_BYTE_LGR_ON_BLK 0x07   /* L-GRAY ON BLACK */
-#define ATTR_BYTE_DGR_ON_BLK 0x08   /* D-GRAY ON BLACK */
-#define ATTR_BYTE_DBL_ON_BLK 0x09   /* DODGER-BLUE ON BLACK */
-
-#define ATTR_BYTE_TEXT_COLOR_STD ATTR_BYTE_LGR_ON_BLK
-
-static char *VIDEO_MEMORY_POINTER = (char*)0xb8000;
-
-static int VIDEO_POINTER_SIZE = 0;
+void kstd_write(const char *__sptr);
+void kstd_writec(const char *__sptr, uint16 __bcolor);
 
 void kstd_clear(void);
-void kstd_putchar(char __c);
-void kstd_write(const char *__sptr);
 
 void printk(const char *__format, ...);
-
-int kstd_strlen(const char *__ptr);
 
 #endif
