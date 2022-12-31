507d61d8cc7674aa87207bd2f8e4adbacf83b8d8
Code-Sploit <64798467+Code-Sploit@users.noreply.github.com>
2022-12-31 04:00:13 -0600

    Move direct interaction with video memory in stdio.c to video driver

--- src/stdio.c
+++ src/stdio.c
@@ -3,145 +3,47 @@
 */
 
 #include "../lib/stdio.h"
-#include "../lib/memory.h"
 #include "../lib/stdlib.h"
+#include "../lib/memory.h"
 
-void kstd_set_vidptr(int __size)
+void kstd_write(const char *__sptr)
 {
-	VIDEO_POINTER_SIZE = __size;
+	unsigned int strlen  = __kstd_strlen(__sptr);
+	unsigned int striter = 0;
+
+	int cursor = __kstd_vga_get_cursor();
+
+	while (striter < strlen)
+	{
+		__kstd_vga_putchar(__sptr[striter], ATTR_BYTE_TEXT_COLOR_STD);
+		__kstd_vga_set_cursor(VIDEO_POINTER_SIZE);
+
+		striter++;
+	}
 }
 
-void kstd_free_space(int __size)
+void kstd_writec(const char *__sptr, uint16 __bcolor)
 {
-	unsigned int cursor = VIDEO_POINTER_SIZE;
+	unsigned int strlen  = __kstd_strlen(__sptr);
+	unsigned int striter = 0;
 
-	while (cursor < __size)
+	while (striter < strlen)
 	{
-		VIDEO_MEMORY_POINTER[cursor] = ' ';
-		VIDEO_MEMORY_POINTER[cursor + 1] = ATTR_BYTE_BLK_ON_BLK;
+		__kstd_vga_putchar(__sptr[striter], __bcolor);
+		__kstd_vga_set_cursor(VIDEO_POINTER_SIZE);
 
-		cursor = cursor + 2;
+		striter++;
 	}
 }
 
 void kstd_clear(void)
 {
-	unsigned int cursor = 0;
-
-	while (cursor < (SCREEN_SIZE))
-	{
-		VIDEO_MEMORY_POINTER[cursor] = ' ';
-		VIDEO_MEMORY_POINTER[cursor + 1] = ATTR_BYTE_BLK_ON_BLK;
-
-		cursor = cursor + 2;
-	}
-
-	VIDEO_POINTER_SIZE = 0;
-}
-
-void kstd_handle_escape(char __p)
-{
-	switch (__p)
-	{
-		case '\n':
-		{
-			int offset_to_eol = (SCREEN_SIZE_COLS * SCREEN_SIZE_BLEN) - (VIDEO_POINTER_SIZE % SCREEN_SIZE_COLS);
-
-			VIDEO_POINTER_SIZE = VIDEO_POINTER_SIZE + offset_to_eol;
-		}
-	}
+	__kstd_vga_clear();
 }
 
 void kstd_backspace(void)
 {
-	VIDEO_POINTER_SIZE = (VIDEO_POINTER_SIZE - 2);
-
-	VIDEO_MEMORY_POINTER[VIDEO_POINTER_SIZE] = ' ';
-	VIDEO_MEMORY_POINTER[VIDEO_POINTER_SIZE + 1] = 0x00;
-}
-
-void kstd_newline(void)
-{
-	int offset = (SCREEN_SIZE_COLS * SCREEN_SIZE_BLEN) - (VIDEO_POINTER_SIZE % SCREEN_SIZE_COLS);
-
-	VIDEO_POINTER_SIZE = (VIDEO_POINTER_SIZE + offset);
-}
-
-void kstd_tab(void)
-{
-	int offset = (SCREEN_SIZE_COLS * SCREEN_SIZE_BLEN) - (VIDEO_POINTER_SIZE % SCREEN_SIZE_COLS);
-
-	if (offset < 4)
-	{
-		int remaining = 4 - offset;
-
-		kstd_newline();
-
-		VIDEO_POINTER_SIZE = (VIDEO_POINTER_SIZE) + remaining;
-
-		return;
-	}
-
-	VIDEO_POINTER_SIZE = (VIDEO_POINTER_SIZE + 4);
-}
-
-void kstd_putchar(char __c)
-{
-	if (__c == '\b')
-	{
-		kstd_backspace();
-
-		return;
-	}
-	
-	VIDEO_MEMORY_POINTER[VIDEO_POINTER_SIZE]     = __c;
-	VIDEO_MEMORY_POINTER[VIDEO_POINTER_SIZE + 1] = 0x07;
-
-	VIDEO_POINTER_SIZE += 2;
-}
-
-void kstd_write(const char *__sptr)
-{
-	unsigned int strlen  = kstd_strlen(__sptr);
-
-	unsigned int viditer = VIDEO_POINTER_SIZE;
-	unsigned int striter = 0;
-
-	kstd_free_space(strlen);
-
-	while (striter < strlen)
-	{
-		if (__sptr[striter] == '\n')
-		{
-			kstd_newline();
-
-			viditer = VIDEO_POINTER_SIZE;
-
-			striter++;
-
-			continue;
-		}
-
-		if (__sptr[striter] == '\t')
-		{
-			kstd_tab();
-
-			viditer = VIDEO_POINTER_SIZE;
-
-			striter++;
-
-			continue;
-		}
-
-		VIDEO_MEMORY_POINTER[viditer] = __sptr[striter];
-		VIDEO_MEMORY_POINTER[viditer + 1] = ATTR_BYTE_TEXT_COLOR_STD;
-
-		viditer = viditer + 2;
-
-		striter++;
-	}
-
-	VIDEO_POINTER_SIZE = viditer;
+	__kstd_vga_backspace();
 }
 
 void printk(const char *__format, ...)
@@ -159,7 +61,7 @@ void printk(const char *__format, ...)
 	{
 		if (c != '%')
 		{
-			kstd_putchar(c);
+			__kstd_vga_putchar(c, ATTR_BYTE_TEXT_COLOR_STD);
 		}
 		else
 		{
@@ -215,16 +117,16 @@ void printk(const char *__format, ...)
 						;
 
 					for (; __p2 < __p1 + pad_y; __p2++)
-						kstd_putchar(pad_x ? '0' : ' ');
+						__kstd_vga_putchar(pad_x ? '0' : ' ', ATTR_BYTE_TEXT_COLOR_STD);
 
 					while (*__p1)
-						kstd_putchar(*__p1);
+						__kstd_vga_putchar(*__p1, ATTR_BYTE_TEXT_COLOR_STD);
 
 					break;
 
 				default:
 				{
-					kstd_putchar(*((int *) __args++));
+					__kstd_vga_putchar(*((int *) __args++), ATTR_BYTE_TEXT_COLOR_STD);
 
 					break;
 				}
